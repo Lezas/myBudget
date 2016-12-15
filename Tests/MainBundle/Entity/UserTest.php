31086641d735c8df2949fa01cc7e81262d21d0a5
Lezas <pkupetis6@gmail.com>
2016-12-15 20:44:04 +0200

    Tests for user entity

--- Tests/MainBundle/Entity/UserTest.php
+++ Tests/MainBundle/Entity/UserTest.php
@@ -27,21 +27,21 @@ class UserTest extends WebTestCase
 
         $user->addCategory($category);
 
-        $this->assertCount(1,$user->getCategories());
-        $this->assertEquals($user,$category->getUser());
+        $this->assertCount(1, $user->getCategories());
+        $this->assertEquals($user, $category->getUser());
 
         $category2 = new Category();
         $category2->setName('Category2');
 
         $user->addCategory($category2);
 
-        $this->assertCount(2,$user->getCategories());
-        $this->assertEquals($user,$category2->getUser());
+        $this->assertCount(2, $user->getCategories());
+        $this->assertEquals($user, $category2->getUser());
 
         $user->removeCategory($category);
 
-        $this->assertCount(1,$user->getCategories());
-        $this->assertEquals(null,$category->getUser());
+        $this->assertCount(1, $user->getCategories());
+        $this->assertEquals(null, $category->getUser());
     }
 
     public function testIncomeFunctionality()
@@ -52,21 +52,21 @@ class UserTest extends WebTestCase
 
         $user->addIncome($income);
 
-        $this->assertCount(1,$user->getIncome());
-        $this->assertEquals($user,$income->getUser());
+        $this->assertCount(1, $user->getIncome());
+        $this->assertEquals($user, $income->getUser());
 
         $income2 = new Income();
         $income2->setName('Income2');
 
         $user->addIncome($income2);
 
-        $this->assertCount(2,$user->getIncome());
-        $this->assertEquals($user,$income2->getUser());
+        $this->assertCount(2, $user->getIncome());
+        $this->assertEquals($user, $income2->getUser());
 
         $user->removeIncome($income);
 
-        $this->assertCount(1,$user->getIncome());
-        $this->assertEquals(null,$income->getUser());
+        $this->assertCount(1, $user->getIncome());
+        $this->assertEquals(null, $income->getUser());
     }
 
     public function testExpenseFunctionality()
@@ -77,20 +77,20 @@ class UserTest extends WebTestCase
 
         $user->addExpense($expense);
 
-        $this->assertCount(1,$user->getExpense());
-        $this->assertEquals($user,$expense->getUser());
+        $this->assertCount(1, $user->getExpense());
+        $this->assertEquals($user, $expense->getUser());
 
         $expense2 = new Expenses();
         $expense2->setName('Expense2');
 
         $user->addExpense($expense2);
 
-        $this->assertCount(2,$user->getExpense());
-        $this->assertEquals($user,$expense2->getUser());
+        $this->assertCount(2, $user->getExpense());
+        $this->assertEquals($user, $expense2->getUser());
 
         $user->removeExpense($expense);
 
-        $this->assertCount(1,$user->getExpense());
-        $this->assertEquals(null,$expense->getUser());
+        $this->assertCount(1, $user->getExpense());
+        $this->assertEquals(null, $expense->getUser());
     }
 }
